29ceffe53e3e15426b3f460d3a968c5935254071
Devin Smith <github@arzynik.com>
2015-09-24 13:19:00 -0700

    added iddleware test

---
+++ tests/MiddlewareTest.php
@@ -0,0 +1,87 @@
+<?php
+
+class LoginServiceMiddleware extends \Tipsy\Middleware {
+	function __construct() {
+		echo 'SERVICECONSTRUCT';
+	}
+	function run($test) {
+		echo 'SERVICERUN'.$test['test'];
+	}
+}
+
+
+class MiddlewareTest extends Tipsy_Test {
+
+	public function setUp() {
+		$this->tip = new Tipsy\Tipsy;
+		$this->useOb = true; // for debug use
+	}
+
+	public function testMiddlewareClass() {
+		$_REQUEST['__url'] = '';
+		$this->ob();
+		$this->tip->middleware('LoginServiceMiddleware', ['test' => 'HI']);
+		$check = $this->ob(false);
+		$this->assertEquals('SERVICECONSTRUCT', $check);
+
+		$this->ob();
+		$this->tip->middleware('LoginServiceMiddleware');
+		$this->tip->router()->home(function() {});
+		$this->tip->start();
+		$check = $this->ob(false);
+		$this->assertEquals('SERVICERUNHI', $check);
+
+		$this->ob();
+		$class = get_class($this->tip->middleware('LoginServiceMiddleware'));
+		$check = $this->ob(false);
+		$this->assertEquals('LoginServiceMiddleware', $class);
+		$this->assertEquals('', $check);
+	}
+
+	public function testMiddlewareTipsy() {
+		$_REQUEST['__url'] = '';
+		$this->ob();
+
+		$this->tip->service('Tipsy\Service/LoginServiceTipsy', [
+			run => function() {
+				echo 'HI';
+			},
+			test => function() {
+				return 'HELLO';
+			}
+		]);
+
+		$this->tip->middleware('LoginServiceTipsy');
+		$check = $this->ob(false);
+		$this->assertEquals('', $check);
+		
+		$this->tip->router()->home(function() {});
+		$this->tip->start();
+		$check = $this->ob(false);
+		$this->assertEquals('HI', $check);
+		
+		$check = $this->tip->service('LoginServiceTipsy')->test();
+		$this->assertEquals('HELLO', $check);
+	}
+	
+	public function testMiddlewareFailure() {
+		$_REQUEST['__url'] = '';
+		$this->ob();
+
+		$this->tip->service('Tipsy\Service/LoginServiceException', [
+			run => function() {
+				return false;
+			}
+		]);
+
+		$this->tip->middleware('LoginServiceException');
+		$this->tip->router()->home(function() {});
+		try {
+			$this->tip->start();
+			$check = false;
+		} catch (Exception $e) {
+			$check = true;
+		}
+		$this->assertTrue($check);
+	}
+}
