e1c21b40bde48215c1fa7b2d95744e9c52b37a6d
Devin Smith <github@arzynik.com>
2015-10-08 14:35:01 -0700

    move header fn

--- src/Tipsy/Tipsy.php
+++ src/Tipsy/Tipsy.php
@@ -92,3 +92,21 @@ class StripWhite extends View_Filter {
 }
 
 
+
+
+
+if (!function_exists('getallheaders'))  {
+	function getallheaders() {
+		if (!is_array($_SERVER)) {
+			return [];
+		}
+
+		$headers = [];
+		foreach ($_SERVER as $name => $value) {
+			if (substr($name, 0, 5) == 'HTTP_') {
+				$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+			}
+		}
+		return $headers;
+	}
+}
