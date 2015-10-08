89282f5cbb5209a899e30dac1c2041103e1120b8
Devin Smith <github@arzynik.com>
2015-10-08 14:38:10 -0700

    fixed two definitions

--- src/Tipsy/Request.php
+++ src/Tipsy/Request.php
@@ -159,20 +159,3 @@ class Request {
 		return $this->_headers;
 	}
 }
-
-
-if (!function_exists('getallheaders'))  {
-	function getallheaders() {
-		if (!is_array($_SERVER)) {
-			return [];
-		}
-
-		$headers = [];
-		foreach ($_SERVER as $name => $value) {
-			if (substr($name, 0, 5) == 'HTTP_') {
-				$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-			}
-		}
-		return $headers;
-	}
-}
