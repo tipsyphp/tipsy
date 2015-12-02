890b9d7b9e0c6e553262af029d94c36cc3e3e48f
Devin Smith <github@arzynik.com>
2015-12-01 17:11:49 -0800

    adde better rest tests

--- tests/RestTest.php
+++ tests/RestTest.php
@@ -2,19 +2,20 @@
 
 
 class RestTest extends Tipsy_Test {
-	
+
 	public function setUp() {
 		$this->tip = new Tipsy\Tipsy;
 		$this->useOb = true; // for debug use
-		
+
 		$this->tip->config('tests/config.ini');
 	}
-	
-	public function testPost() {
+
+	public function testFormPost() {
 		$_REQUEST['__url'] = 'test';
 		$_SERVER['REQUEST_METHOD'] = 'POST';
+		$_SERVER['CONTENT_TYPE'] = 'application/x-www-form-urlencoded';
 		$_POST['data'] = 'blah';
-		
+
 		$this->tip->service('TestModel', []);
 
 		$this->tip->router()
@@ -23,18 +24,19 @@ class RestTest extends Tipsy_Test {
 				$TestModel->data = $Request->data;
 				echo $TestModel->json();
 			});
-		
+
 		$this->ob();
 		$this->tip->start();
 		$check = $this->ob(false);
 		$this->assertEquals(json_encode(['test' => 'hi', 'data' => 'blah']), $check);
 	}
-	
-	public function testGet() {
+
+	public function testFormGet() {
 		$_REQUEST['__url'] = 'test';
 		$_SERVER['REQUEST_METHOD'] = 'GET';
+		$_SERVER['CONTENT_TYPE'] = 'application/x-www-form-urlencoded';
 		$_GET['data'] = 'blah';
-		
+
 		$this->tip->service('TestModel', []);
 
 		$this->tip->router()
@@ -43,17 +45,20 @@ class RestTest extends Tipsy_Test {
 				$TestModel->data = $Request->data;
 				echo $TestModel->json();
 			});
-		
+
 		$this->ob();
 		$this->tip->start();
 		$check = $this->ob(false);
 		$this->assertEquals(json_encode(['test' => 'hi', 'data' => 'blah']), $check);
 	}
-	
-	public function testDelete() {
+
+	public function testFormDelete() {
 		$_REQUEST['__url'] = 'test/1';
+		$_SERVER['CONTENT_TYPE'] = 'application/x-www-form-urlencoded';
 		$_SERVER['REQUEST_METHOD'] = 'DELETE';
-		
+		$_ENV['TESTS_PHP_INPUT'] = '/tmp/phpinput';
+		file_put_contents($_ENV['TESTS_PHP_INPUT'], '');
+
 		$this->tip->service('TestModel', []);
 
 		$this->tip->router()
@@ -62,10 +67,117 @@ class RestTest extends Tipsy_Test {
 				$TestModel->id = $Params->id;
 				echo $TestModel->json();
 			});
-		
+
 		$this->ob();
 		$this->tip->start();
 		$check = $this->ob(false);
 		$this->assertEquals(json_encode(['test' => 'hi', 'id' => '1']), $check);
+	}
+
+	public function testFormGeneric() {
+		$_REQUEST['__url'] = 'test';
+		$_SERVER['CONTENT_TYPE'] = 'application/x-www-form-urlencoded';
+		$_SERVER['REQUEST_METHOD'] = 'GENERIC';
+		$_ENV['TESTS_PHP_INPUT'] = '/tmp/phpinput';
+		file_put_contents($_ENV['TESTS_PHP_INPUT'], 'data=blah&something=else');
+
+		$this->tip->service('TestModel', []);
+
+		$this->tip->router()
+			->generic('test',function($TestModel, $Request) {
+				$TestModel->test = 'hi';
+				$TestModel->data = $Request->data;
+				echo $TestModel->json();
+			});
+
+		$this->ob();
+		$this->tip->start();
+		$check = $this->ob(false);
+		$this->assertEquals(json_encode(['test' => 'hi', 'data' => 'blah']), $check);
+	}
+
+	public function testJsonPost() {
+		$_REQUEST['__url'] = 'test';
+		$_SERVER['REQUEST_METHOD'] = 'POST';
+		$_SERVER['CONTENT_TYPE'] = 'application/json';
+		$_ENV['TESTS_PHP_INPUT'] = '/tmp/phpinput';
+		file_put_contents($_ENV['TESTS_PHP_INPUT'], '{"data": "blah"}');
+
+		$this->tip->service('TestModel', []);
+
+		$this->tip->router()
+			->post('test',function($TestModel, $Request) {
+				$TestModel->test = 'hi';
+				$TestModel->data = $Request->data;
+				echo $TestModel->json();
+			});
+
+		$this->ob();
+		$this->tip->start();
+		$check = $this->ob(false);
+		$this->assertEquals(json_encode(['test' => 'hi', 'data' => 'blah']), $check);
+	}
+
+	public function testJsonGet() {
+		$_REQUEST['__url'] = 'test';
+		$_SERVER['REQUEST_URI'] = '/?{"data": "blah"}';
+		$_SERVER['REQUEST_METHOD'] = 'GET';
+		$_SERVER['CONTENT_TYPE'] = 'application/json';
+
+		$this->tip->service('TestModel', []);
+
+		$this->tip->router()
+			->get('test',function($TestModel, $Request) {
+				$TestModel->test = 'hi';
+				$TestModel->data = $Request->data;
+				echo $TestModel->json();
+			});
+
+		$this->ob();
+		$this->tip->start();
+		$check = $this->ob(false);
+		$this->assertEquals(json_encode(['test' => 'hi', 'data' => 'blah']), $check);
+	}
+
+	public function testJsonDelete() {
+		$_REQUEST['__url'] = 'test/1';
+		$_SERVER['CONTENT_TYPE'] = 'application/json';
+		$_SERVER['REQUEST_METHOD'] = 'DELETE';
+
+		$this->tip->service('TestModel', []);
+
+		$this->tip->router()
+			->delete('test/:id',function($Params, $TestModel) {
+				$TestModel->test = 'hi';
+				$TestModel->id = $Params->id;
+				echo $TestModel->json();
+			});
+
+		$this->ob();
+		$this->tip->start();
+		$check = $this->ob(false);
+		$this->assertEquals(json_encode(['test' => 'hi', 'id' => '1']), $check);
+	}
+
+	public function testJsonGeneric() {
+		$_REQUEST['__url'] = 'test';
+		$_SERVER['CONTENT_TYPE'] = 'application/json';
+		$_SERVER['REQUEST_METHOD'] = 'GENERIC';
+		$_ENV['TESTS_PHP_INPUT'] = '/tmp/phpinput';
+		file_put_contents($_ENV['TESTS_PHP_INPUT'], '{"data": "blah"}');
+
+		$this->tip->service('TestModel', []);
+
+		$this->tip->router()
+			->generic('test',function($TestModel, $Request) {
+				$TestModel->test = 'hi';
+				$TestModel->data = $Request->data;
+				echo $TestModel->json();
+			});
+
+		$this->ob();
+		$this->tip->start();
+		$check = $this->ob(false);
+		$this->assertEquals(json_encode(['test' => 'hi', 'data' => 'blah']), $check);
 	}
 }
